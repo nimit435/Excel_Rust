643aeff86a35834cce1cec842f62af24a2f7f9a9
nimit435 <nimitjain435@gmail.com>
2025-07-20 20:26:00 +0530

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use Excel_Rust::{display::display_sheet, parsing, skeleton::Sheet};
 
 fn main() {
-    let sheet = Sheet::create_sheet(10, 10);
+    let mut sheet = Sheet::create_sheet(10, 10);
+    display_sheet(&sheet);
+    sheet.matrix[0].is_valid = false;
     display_sheet(&sheet);
 
-
-    let input = "A1=4+-1";
+    let input = "A1=SUM(C1:B2)";
 
     match parsing::parse_input(&input, &sheet) {
         Ok(_) => println!(),
